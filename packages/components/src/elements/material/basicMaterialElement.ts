ad80504f3372851586d5488e3a02074197b8c4d1
Brandon <brndnchong@gmail.com>
2025-12-31 11:59:43 -0800

    Add styling

--- packages/components/src/elements/material/basicMaterialElement.ts
+++ packages/components/src/elements/material/basicMaterialElement.ts
@@ -1,5 +1,8 @@
-import { MaterialElement } from "./materialElement";
 import { MeshBasicMaterial } from "three";
+import { createStyleObserver } from "../styleObserver";
+import { MaterialElement } from "./materialElement";
+
+const styleProperties = ["visibility", "opacity", "color"];
 
 export class BasicMaterialElement extends MaterialElement {
   private _material = new MeshBasicMaterial({
@@ -11,6 +14,34 @@ export class BasicMaterialElement extends MaterialElement {
 
     if (this.target) {
       this.target.material = this._material;
+    }
+
+    createStyleObserver({
+      element: this,
+      properties: styleProperties,
+      onUpdate: this.onStyleUpdate.bind(this),
+      signal: this.connectedSignal,
+    });
+  }
+
+  private onStyleUpdate(property: string, value: string) {
+    switch (property) {
+      case "visibility": {
+        this._material.visible = value === "visible";
+        break;
+      }
+      case "opacity": {
+        this._material.opacity = Number.parseFloat(value);
+        this._material.transparent = this._material.opacity !== 1;
+        this._material.needsUpdate = true;
+        break;
+      }
+      case "color": {
+        this._material.color.set(value);
+        break;
+      }
+      default:
+        break;
     }
   }
 }
--- packages/components/src/elements/nodes/sceneElement.ts
+++ packages/components/src/elements/nodes/sceneElement.ts
@@ -1,5 +1,8 @@
+import { Color, Scene } from "three";
 import { NodeElement } from "./nodeElement";
-import { Scene } from "three";
+import { createStyleObserver } from "../styleObserver";
+
+const styleProperties = ["background-color"];
 
 export class SceneElement extends NodeElement<Scene> {
   constructor() {
@@ -12,6 +15,25 @@ export class SceneElement extends NodeElement<Scene> {
     const context = this.getRootContext();
     if (context) {
       context.createScene(this.object);
+    }
+
+    createStyleObserver({
+      element: this,
+      properties: styleProperties,
+      onUpdate: this.onStyleUpdate.bind(this),
+      signal: this.connectedSignal,
+    });
+  }
+
+  private onStyleUpdate(property: string, value: string) {
+    switch (property) {
+      case "background-color": {
+        this.object.background = new Color(value);
+
+        break;
+      }
+      default:
+        break;
     }
   }
 }
