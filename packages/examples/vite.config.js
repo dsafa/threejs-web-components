555137884f6f11e7b7446f63cb3be663fbb7f3cb
Brandon <brndnchong@gmail.com>
2026-01-04 16:22:09 -0800

    Base

--- packages/examples/vite.config.js
+++ packages/examples/vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/threejs-web-components/",
   build: {
     rolldownOptions: {
       input: {
